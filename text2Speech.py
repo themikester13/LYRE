4e5043e486298585f984a955b12e53ea91288fb1
themikester13 <mjiang321@gmail.com>
2019-03-16 12:03:07 -0500

    finished making whole lines appear in the video

--- text2Speech.py
+++ text2Speech.py
@@ -148,10 +148,13 @@ def approximate(startEndTime, lyric):
 	return syllableStartEndTime
 
 #process wav file
-def onsetModel(filepath, wordStartEndTime):
+def onsetModel(filepath, wordStartEndTime, lyrics):
 	sr = 44100
 	syllModel = []
 	dic = pyphen.Pyphen(lang='en')
+	currLyricLine = 0
+	moveToNextLine = False
+	wordPos = 0
 	for word in wordStartEndTime:
 #		print(word)
 		start = word['start']
@@ -159,9 +162,13 @@ def onsetModel(filepath, wordStartEndTime):
 		#get number of syllables, i.e. onsets, we want
 		syllables = (dic.inserted(word["word"])).split("-")
 		desiredOnsets = len(syllables)
+		currLine = lyrics[currLyricLine].split(" ")
+		if wordPos == len(currLine) - 1:
+			moveToNextLine = True
 		#if it is single syllable
 		if desiredOnsets == 1:
-			syllModel.append(((start, end), syllables[0]))
+			emphasizedLine = " ".join(currLine[:wordPos] + ["***" + syllables[0] +"***"] + currLine[wordPos+1:])
+			syllModel.append(((start, end), emphasizedLine))
 		#if we have multiple syllables
 		else:
 			#get audio signal
@@ -194,10 +201,22 @@ def onsetModel(filepath, wordStartEndTime):
 				times = librosa.frames_to_time(nHighestFrames, sr = sr)
 				
 				for i in range(desiredOnsets-1):
+					#calculate the start end times of the syllables
 					syllStart = start + times[i]
 					syllEnd = start+times[i+1]
-					syllModel.append(((syllStart, syllEnd), syllables[i]))
-				syllModel.append(((start + times[desiredOnsets-1], end), syllables[desiredOnsets-1]))
+					#calculate the emphasized lyrics
+					emphasizedLine = " ".join(currLine[:wordPos] + syllables[:i] +["***" + syllables[i] + "***"] + syllables[i+1:] + currLine[wordPos+1:])
+					syllModel.append(((syllStart, syllEnd), emphasizedLine))
+				emphasizedLine = " ".join(currLine[:wordPos] + syllables[:desiredOnsets-1] +["***" + syllables[desiredOnsets-1] + "***"] + currLine[wordPos+1:])
+
+				syllModel.append(((start + times[desiredOnsets-1], end), emphasizedLine))
+		#if we are moving to next line, move to next line and reset the counter
+		if moveToNextLine:
+			moveToNextLine = False
+			currLyricLine += 1
+			wordPos = 0
+		else:
+			wordPos += 1			
 	return syllModel
 	
 
@@ -261,7 +280,7 @@ def makeVideoWithAudio(filepath, filename):
 def main():
 	loadCredentials()
 	songName = "FixYou"
-	model = "approx"
+	model = "onset"
 	filepath = getFilePath(songName + ".wav")
 	startEndTime, lyrics = getSpeechInfo(filepath)
 #	print(startEndTime)
@@ -269,16 +288,16 @@ def main():
 #	lyrics = ["when you try your best but you don't succeed", 'when you get what you want but not watching', "when you feel so tired but you can't sleep", 'cheer skirts']
 
 #	print(lyrics)	
-	modelTimes = approximate(startEndTime, lyrics)
-	print(modelTimes)
+#	modelTimes = approximate(startEndTime, lyrics)
 
-#	modelTimes = onsetModel(filepath, startEndTime)
+	modelTimes = onsetModel(filepath, startEndTime, lyrics)
+	print(modelTimes)
 #	approx = [{'syll': 'when', 'start': 0.0, 'end': 0.4}, {'syll': 'you', 'start': 0.4, 'end': 0.5}, {'syll': 'try', 'start': 0.5, 'end': 1.0}, {'syll': 'your', 'start': 1.0, 'end': 1.4}, {'syll': 'best', 'start': 1.4, 'end': 1.7}, {'syll': 'but', 'start': 1.7, 'end': 2.3}, {'syll': 'you', 'start': 2.3, 'end': 2.5}, {'syll': "don't", 'start': 2.5, 'end': 2.7}, {'syll': 'suc', 'start': 2.7, 'end': 2.9000000000000004}, {'syll': 'ceed', 'start': 2.9000000000000004, 'end': 3.1}, {'syll': 'when', 'start': 7.0, 'end': 7.5}, {'syll': 'you', 'start': 7.5, 'end': 7.6}, {'syll': 'get', 'start': 7.6, 'end': 8.0}, {'syll': 'what', 'start': 8.0, 'end': 8.4}, {'syll': 'you', 'start': 8.4, 'end': 8.7}, {'syll': 'want', 'start': 8.7, 'end': 9.4}, {'syll': 'but', 'start': 9.4, 'end': 9.5}, {'syll': 'not', 'start': 9.5, 'end': 9.8}, {'syll': 'watch', 'start': 9.8, 'end': 10.15}, {'syll': 'ing', 'start': 10.15, 'end': 10.5}, {'syll': 'when', 'start': 14.0, 'end': 14.5}, {'syll': 'you', 'start': 14.5, 'end': 14.6}, {'syll': 'feel', 'start': 14.6, 'end': 14.9}, {'syll': 'so', 'start': 14.9, 'end': 15.6}, {'syll': 'tired', 'start': 15.6, 'end': 15.8}, {'syll': 'but', 'start': 15.8, 'end': 16.6}, {'syll': 'you', 'start': 16.6, 'end': 16.9}, {'syll': "can't", 'start': 16.9, 'end': 17.0}, {'syll': 'sleep', 'start': 17.0, 'end': 17.6}, {'syll': 'cheer', 'start': 28.1, 'end': 29.2}, {'syll': 'skirts', 'start': 29.2, 'end': 29.9}]
 #	approx = [((0.0, 0.4), 'when'), ((0.4, 0.5), 'you'), ((0.5, 1.0), 'try'), ((1.0, 1.4), 'your'), ((1.4, 1.7), 'best'), ((1.7, 2.3), 'but'), ((2.3, 2.5), 'you'), ((2.5, 2.7), "don't"), ((2.7, 2.9000000000000004), 'suc'), ((2.9000000000000004, 3.1), 'ceed'), ((7.0, 7.5), 'when'), ((7.5, 7.6), 'you'), ((7.6, 8.0), 'get'), ((8.0, 8.4), 'what'), ((8.4, 8.7), 'you'), ((8.7, 9.4), 'want'), ((9.4, 9.5), 'but'), ((9.5, 9.8), 'not'), ((9.8, 10.15), 'watch'), ((10.15, 10.5), 'ing'), ((14.0, 14.5), 'when'), ((14.5, 14.6), 'you'), ((14.6, 14.9), 'feel'), ((14.9, 15.6), 'so'), ((15.6, 15.8), 'tired'), ((15.8, 16.6), 'but'), ((16.6, 16.9), 'you'), ((16.9, 17.0), "can't"), ((17.0, 17.6), 'sleep'), ((28.1, 29.2), 'cheer'), ((29.2, 29.9), 'skirts')] 
 #	print(modelTimes)
 #	print("start end time", startEndTime)
-	videoFile = makeVideoWithAudio(filepath, songName)	
-#	videoFile = './movieFiles/Karaoke/' + songName+ '.avi'
+#	videoFile = makeVideoWithAudio(filepath, songName)	
+	videoFile = './movieFiles/Karaoke/' + songName+ '.avi'
 	videoLen = findLengthOfAudio(filepath)
 	addSubtitles(modelTimes, videoFile, songName +"AnnotatedVid" + model, videoLen)
 main()
