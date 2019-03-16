c5583fbe6beadded8b2a93a543119235aa384097
themikester13 <mjiang321@gmail.com>
2019-03-15 22:32:53 -0500

    displays the entire line

--- text2Speech.py
+++ text2Speech.py
@@ -84,34 +84,67 @@ def getSpeechInfo(path2File):
 
 #	print("the type is: ", type(response))
 	startEndTime = []
+	transcript = []
 	for result in response.results:
 		wordList = result.alternatives[0].words
+		transcript.append(result.alternatives[0].transcript)
 		for wordInfo in wordList:
 			startEndTime.append(convertToPython(wordInfo))
-	return startEndTime
+	transcript = [t[1:] if t[0] == " " else t for t in transcript]				
+	return startEndTime, transcript
+
+#returns the lyricPosition
+def wordPos(word, lyrics):
+	for i in range(len(lyrics)):
+		if word == lyrics[i]:
+			return i
+	return 0
 
 #Approximate Algorithm
-def approximate(startEndTime):
+def approximate(startEndTime, lyric):
 	syllableStartEndTime = []
 	#print(startEndTime)
 	dic = pyphen.Pyphen(lang='en')
+	currLyricLine = 0
+	wordPosition = 0
+	nextLine = False
 	for word in startEndTime:	
+		#split into syllables
 		syllables = (dic.inserted(word["word"])).split("-")
+		#find start and end times of the word
 		start = word["start"]
 		end = word["end"]
 		timeDuration = end-start
+		#calculate start and end of syllable times
 		if(len(syllables) == 0):
 			timeStep = timeDuration/float(1)
 		else:
 			timeStep = timeDuration/float(len(syllables))
+		#the list of words in the line
+		lyricLine = lyric[currLyricLine].split(" ")
+#		print(lyricLine)
+		#find where the word occurs in the line
+		#if its the last word, increment to the next line
+		if wordPosition == len(lyricLine) - 1:
+			currLyricLine += 1
+			nextLine = True
 		count = 0
-		for syll in syllables:
+		for i in range(len(syllables)):
+			syll = syllables[i]
 			syllStart = start + timeStep * count
 			syllEnd = start + timeStep * (count+1)
-			formattedData = ((syllStart, syllEnd), syll)
+#			print(lyricLine[:wordPosition])
+			textEmphasis = " ".join(lyricLine[:wordPosition] + ["".join(syllables[:i] + ["***" + syll + "***"] + syllables[i+1:])] + lyricLine[wordPosition+1:])
+			print(textEmphasis)
+#			print("The WORD position is: ", wordPosition)
+			formattedData = ((syllStart, syllEnd), textEmphasis)
 			syllableStartEndTime.append(formattedData)
-			#syllableStartEndTime.append({'syll': syll, 'start': syllStart, 'end': syllEnd})
 			count += 1
+		if nextLine:
+			nextLine = False
+			wordPosition = 0
+		else:
+			wordPosition += 1
 	return syllableStartEndTime
 
 #process wav file
@@ -178,7 +211,7 @@ def findLengthOfAudio(filepath):
 def addSubtitles(startEndTimes, filepath, filename, vidLen):
 	video = VideoFileClip(filepath)
 	currVideo = video
-	generator = lambda text: TextClip(text, font="Times", fontsize=400, color='white').set_pos('center')
+	generator = lambda text: TextClip(text, font="Times", fontsize=150, color='white').set_pos('center')
 	sub = SubtitlesClip(startEndTimes, generator)
 	final = CompositeVideoClip([video, sub.set_pos(('center', 'center'))])
 	path = './movieFiles/Karaoke/' + filename + '.avi'
@@ -228,20 +261,24 @@ def makeVideoWithAudio(filepath, filename):
 def main():
 	loadCredentials()
 	songName = "FixYou"
-	model = "onset"
+	model = "approx"
 	filepath = getFilePath(songName + ".wav")
-	startEndTime = getSpeechInfo(filepath)
-	print(startEndTime)
-#	approx = approximate(startEndTime)
-#	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5, 'end': 1.0}, {'word': 'your', 'start': 1.0, 'end': 1.4}, {'word': 'best', 'start': 1.4, 'end': 1.7}, {'word': 'but', 'start': 1.7, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but', 'start': 9.4, 'end': 9.5}, {'word': 'not', 'start': 9.5, 'end': 9.8}, {'word': 'watching', 'start': 9.8, 'end': 10.5}, {'word': 'when', 'start': 14.0, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 14.9}, {'word': 'so', 'start': 14.9, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.6}, {'word': 'you', 'start': 16.6, 'end': 16.9}, {'word': "can't", 'start': 16.9, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.6}, {'word': 'cheer', 'start': 28.1, 'end': 29.2}, {'word': 'skirts', 'start': 29.2, 'end': 29.9}]
+	startEndTime, lyrics = getSpeechInfo(filepath)
+#	print(startEndTime)
+#	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5    , 'end': 1.0}, {'word': 'your', 'start': 1.0, 'end': 1.4}, {'word': 'best', 'start': 1.4, 'end': 1.7}, {'word': 'but', 'start': 1.7, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but'    , 'start': 9.4, 'end': 9.5}, {'word': 'not', 'start': 9.5, 'end': 9.8}, {'word': 'watching', 'start': 9.8, 'end': 10.5}, {'word': 'when', 'start': 14.0, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 14.9}, {'word': 'so', 'start': 14.9, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.6}, {'word': 'you', 'start': 16.6, 'end': 16.9}, {'word': "can't", 'start': 16.9, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.6}, {'word': 'cheer', 'start': 28.1, 'end': 29.2}, {'word': 'skirts', 'start': 29.2, 'end': 29.9}]
+#	lyrics = ["when you try your best but you don't succeed", 'when you get what you want but not watching', "when you feel so tired but you can't sleep", 'cheer skirts']
+
+#	print(lyrics)	
+	modelTimes = approximate(startEndTime, lyrics)
+	print(modelTimes)
+
 #	modelTimes = onsetModel(filepath, startEndTime)
 #	approx = [{'syll': 'when', 'start': 0.0, 'end': 0.4}, {'syll': 'you', 'start': 0.4, 'end': 0.5}, {'syll': 'try', 'start': 0.5, 'end': 1.0}, {'syll': 'your', 'start': 1.0, 'end': 1.4}, {'syll': 'best', 'start': 1.4, 'end': 1.7}, {'syll': 'but', 'start': 1.7, 'end': 2.3}, {'syll': 'you', 'start': 2.3, 'end': 2.5}, {'syll': "don't", 'start': 2.5, 'end': 2.7}, {'syll': 'suc', 'start': 2.7, 'end': 2.9000000000000004}, {'syll': 'ceed', 'start': 2.9000000000000004, 'end': 3.1}, {'syll': 'when', 'start': 7.0, 'end': 7.5}, {'syll': 'you', 'start': 7.5, 'end': 7.6}, {'syll': 'get', 'start': 7.6, 'end': 8.0}, {'syll': 'what', 'start': 8.0, 'end': 8.4}, {'syll': 'you', 'start': 8.4, 'end': 8.7}, {'syll': 'want', 'start': 8.7, 'end': 9.4}, {'syll': 'but', 'start': 9.4, 'end': 9.5}, {'syll': 'not', 'start': 9.5, 'end': 9.8}, {'syll': 'watch', 'start': 9.8, 'end': 10.15}, {'syll': 'ing', 'start': 10.15, 'end': 10.5}, {'syll': 'when', 'start': 14.0, 'end': 14.5}, {'syll': 'you', 'start': 14.5, 'end': 14.6}, {'syll': 'feel', 'start': 14.6, 'end': 14.9}, {'syll': 'so', 'start': 14.9, 'end': 15.6}, {'syll': 'tired', 'start': 15.6, 'end': 15.8}, {'syll': 'but', 'start': 15.8, 'end': 16.6}, {'syll': 'you', 'start': 16.6, 'end': 16.9}, {'syll': "can't", 'start': 16.9, 'end': 17.0}, {'syll': 'sleep', 'start': 17.0, 'end': 17.6}, {'syll': 'cheer', 'start': 28.1, 'end': 29.2}, {'syll': 'skirts', 'start': 29.2, 'end': 29.9}]
 #	approx = [((0.0, 0.4), 'when'), ((0.4, 0.5), 'you'), ((0.5, 1.0), 'try'), ((1.0, 1.4), 'your'), ((1.4, 1.7), 'best'), ((1.7, 2.3), 'but'), ((2.3, 2.5), 'you'), ((2.5, 2.7), "don't"), ((2.7, 2.9000000000000004), 'suc'), ((2.9000000000000004, 3.1), 'ceed'), ((7.0, 7.5), 'when'), ((7.5, 7.6), 'you'), ((7.6, 8.0), 'get'), ((8.0, 8.4), 'what'), ((8.4, 8.7), 'you'), ((8.7, 9.4), 'want'), ((9.4, 9.5), 'but'), ((9.5, 9.8), 'not'), ((9.8, 10.15), 'watch'), ((10.15, 10.5), 'ing'), ((14.0, 14.5), 'when'), ((14.5, 14.6), 'you'), ((14.6, 14.9), 'feel'), ((14.9, 15.6), 'so'), ((15.6, 15.8), 'tired'), ((15.8, 16.6), 'but'), ((16.6, 16.9), 'you'), ((16.9, 17.0), "can't"), ((17.0, 17.6), 'sleep'), ((28.1, 29.2), 'cheer'), ((29.2, 29.9), 'skirts')] 
 #	print(modelTimes)
 #	print("start end time", startEndTime)
-#	videoFile = makeVideoWithAudio(filepath, songName)	
+	videoFile = makeVideoWithAudio(filepath, songName)	
 #	videoFile = './movieFiles/Karaoke/' + songName+ '.avi'
-#	videoLen = findLengthOfAudio(filepath)
-#	generateTextClips(approx, videoFile, "annotatedVid", videoLen)
-#	addSubtitles(modelTimes, videoFile, songName +"AnnotatedVid" + model, videoLen)
+	videoLen = findLengthOfAudio(filepath)
+	addSubtitles(modelTimes, videoFile, songName +"AnnotatedVid" + model, videoLen)
 main()
