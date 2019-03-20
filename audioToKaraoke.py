2adc9895a73f4f5c9f1db92ad5915d568a3a6126
themikester13 <mjiang321@gmail.com>
2019-03-20 16:31:03 -0500

    restructured approx and onset

--- audioToKaraoke.py
+++ audioToKaraoke.py
@@ -174,118 +174,96 @@ def generateLines(audio, startEndTime):
 	return linesStartEnd, lines
 
 #Approximate Algorithm returns a list of tuple of tuple ((start, end), lyric)
-def approximate(startEndTime, lyric):
+def approximate(lineStartEndTime):
 	syllableStartEndTime = []
 	#print(startEndTime)
 	dic = pyphen.Pyphen(lang='en')
-	currLyricLine = 0
-	wordPosition = 0
-	nextLine = False
-	for word in startEndTime:	
-		#split into syllables
-		syllables = (dic.inserted(word["word"])).split("-")
-		#find start and end times of the word
-		start = word["start"]
-		end = word["end"]
-		timeDuration = end-start
-		#calculate start and end of syllable times
-		if(len(syllables) == 0):
-			timeStep = timeDuration/float(1)
-		else:
-			timeStep = timeDuration/float(len(syllables))
-		#the list of words in the line
-		lyricLine = lyric[currLyricLine].split(" ")
-		#find where the word occurs in the line
-		#if its the last word, increment to the next line
-		if wordPosition == len(lyricLine) - 1:
-			currLyricLine += 1
-			nextLine = True
-		count = 0
-		for i in range(len(syllables)):
-			syll = syllables[i]
-			syllStart = start + timeStep * count
-			syllEnd = start + timeStep * (count+1)
-			textEmphasis = " ".join(lyricLine[:wordPosition] + ["".join(syllables[:i] + ["***" + syll + "***"] + syllables[i+1:])] + lyricLine[wordPosition+1:])
-			formattedData = ((syllStart, syllEnd), textEmphasis)
-			syllableStartEndTime.append(formattedData)
-			count += 1
-		if nextLine:
-			nextLine = False
-			wordPosition = 0
-		else:
-			wordPosition += 1
+	for line in lineStartEndTime:
+		lyricLine = [word["word"] for word in line]
+		for wordPos, word in enumerate(line):	
+			#split into syllables
+			syllables = (dic.inserted(word["word"])).split("-")
+			#find start and end times of the word
+			start = word["start"]
+			end = word["end"]
+			timeDuration = end-start
+			#calculate start and end of syllable times
+			if(len(syllables) == 0):
+				timeStep = timeDuration/float(1)
+			else:
+				timeStep = timeDuration/float(len(syllables))
+			#find where the word occurs in the line
+			#if its the last word, increment to the next line
+			count = 0
+			for i in range(len(syllables)):
+				syll = syllables[i]
+				syllStart = start + timeStep * count
+				syllEnd = start + timeStep * (count+1)
+				textEmphasis = " ".join(lyricLine[:wordPos] + ["".join(syllables[:i] + ["***" + syll + "***"] + syllables[i+1:])] + lyricLine[wordPos+1:])
+				formattedData = ((syllStart, syllEnd), textEmphasis)
+				syllableStartEndTime.append(formattedData)
 	return syllableStartEndTime
 
 #process wav file for onsets. Returns ((start, end), lyric)
-def onsetModel(filepath, wordStartEndTime, lyrics):
+def onsetModel(filepath, lineStartEndTime):
 	sr = 44100
 	syllModel = []
 	dic = pyphen.Pyphen(lang='en')
 	currLyricLine = 0
-	moveToNextLine = False
-	wordPos = 0
-	for word in wordStartEndTime:
-		start = word['start']
-		end = word['end']
-		duration = end-start
-		#get number of syllables, i.e. onsets, we want
-		syllables = (dic.inserted(word["word"])).split("-")
-		desiredOnsets = len(syllables)
-		currLine = lyrics[currLyricLine].split(" ")
-		if wordPos == len(currLine) - 1:
-			moveToNextLine = True
-		#if it is single syllable
-		if desiredOnsets == 1 or duration == 0:
-			emphasizedLine = " ".join(currLine[:wordPos] + ["***" + syllables[0] +"***"] + currLine[wordPos+1:])
-			syllModel.append(((start, end), emphasizedLine))
+	for line in lineStartEndTime:
+		sepLine = [wordStartEndTime['word'] for wordStartEndTime in line]
+		for wordPos, word in enumerate(line):
+			start = word['start']
+			end = word['end']
+			duration = end-start
+			#get number of syllables, i.e. onsets, we want
+			syllables = (dic.inserted(word["word"])).split("-")
+			desiredOnsets = len(syllables)
+			#if it is single syllable
+			if desiredOnsets == 1 or duration == 0:
+				emphasizedLine = " ".join(sepLine[:wordPos] + ["***" + syllables[0] +"***"] + sepLine[wordPos+1:])
+				syllModel.append(((start, end), emphasizedLine))
 		#if we have multiple syllables
-		else:
-			#get audio signal
-			audio, sr = librosa.load(filepath, offset = start, duration = duration)
+			else:
+				#get audio signal
+				audio, sr = librosa.load(filepath, offset = start, duration = duration)
 	
-			#perform onset detection returns n onsets
-			onsetFrames = librosa.onset.onset_detect(y = audio, sr = sr)
-			
-			#if the number of onsets is less than the desired n, we assume their length is the same
-			if len(onsetFrames) < desiredOnsets:
-				currStart = start
-				stepSize = (end - start)/desiredOnsets
-				for syll in syllables:
-					syllModel.append(((currStart, currStart+stepSize), syll))
-					currStart += stepSize
+				#perform onset detection returns n onsets
+				onsetFrames = librosa.onset.onset_detect(y = audio, sr = sr)
+				
+				#if the number of onsets is less than the desired n, we assume their length is the same
+				if len(onsetFrames) < desiredOnsets:
+					currStart = start
+					stepSize = (end - start)/desiredOnsets
+					for syll in syllables:
+						syllModel.append(((currStart, currStart+stepSize), syll))
+						currStart += stepSize
 
 			#else, we need to find n highest peaks
-			else:
+				else:
 				#compute onset strength on the section
-				onsetStrengths = librosa.onset.onset_strength(y = audio, sr = sr)
+					onsetStrengths = librosa.onset.onset_strength(y = audio, sr = sr)
 				
-				#corresponding strength to frames
-				strengthToFrames = []
-				for onsetFrame in onsetFrames:
-					strengthToFrames.append((onsetStrengths[onsetFrame], onsetFrame))
-				#sort in descending order
-				sortedFrames = np.sort(strengthToFrames)[::1]
-				#extract the highest frames and find the appropriate time
-				nHighestFrames = [frame[1] for frame in sortedFrames[:desiredOnsets]]
-				times = librosa.frames_to_time(nHighestFrames, sr = sr)
+					#corresponding strength to frames
+					strengthToFrames = []
+					for onsetFrame in onsetFrames:
+						strengthToFrames.append((onsetStrengths[onsetFrame], onsetFrame))
+					#sort in descending order
+					sortedFrames = np.sort(strengthToFrames)[::1]
+					#extract the highest frames and find the appropriate time
+					nHighestFrames = [frame[1] for frame in sortedFrames[:desiredOnsets]]
+					times = librosa.frames_to_time(nHighestFrames, sr = sr)
 				
-				for i in range(desiredOnsets-1):
-					#calculate the start end times of the syllables
-					syllStart = start + times[i]
-					syllEnd = start+times[i+1]
-					#calculate the emphasized lyrics
-					emphasizedLine = " ".join(currLine[:wordPos] + syllables[:i] +["***" + syllables[i] + "***"] + syllables[i+1:] + currLine[wordPos+1:])
-					syllModel.append(((syllStart, syllEnd), emphasizedLine))
-				emphasizedLine = " ".join(currLine[:wordPos] + syllables[:desiredOnsets-1] +["***" + syllables[desiredOnsets-1] + "***"] + currLine[wordPos+1:])
+					for i in range(desiredOnsets-1):
+						#calculate the start end times of the syllables
+						syllStart = start + times[i]
+						syllEnd = start+times[i+1]
+						#calculate the emphasized lyrics
+						emphasizedLine = " ".join(sepLine[:wordPos] + syllables[:i] +["***" + syllables[i] + "***"] + syllables[i+1:] + sepLine[wordPos+1:])
+						syllModel.append(((syllStart, syllEnd), emphasizedLine))
+					emphasizedLine = " ".join(sepLine[:wordPos] + syllables[:desiredOnsets-1] +["***" + syllables[desiredOnsets-1] + "***"] + sepLine[wordPos+1:])
 
-				syllModel.append(((start + times[desiredOnsets-1], end), emphasizedLine))
-		#if we are moving to next line, move to next line and reset the counter
-		if moveToNextLine:
-			moveToNextLine = False
-			currLyricLine += 1
-			wordPos = 0
-		else:
-			wordPos += 1			
+					syllModel.append(((start + times[desiredOnsets-1], end), emphasizedLine))
 	return syllModel
 	
 #finds the length of audio
@@ -366,8 +344,9 @@ def runModel(songName, model):
 	# plot_audio(audio, sr, figsize=(16,4), title="I'm Yours")
 
 	# execute transcription
-	startEndTime, lyrics = getSpeechInfo(filepath)
-#	print(startEndTime)
+#	startEndTime, lyrics = getSpeechInfo(filepath)
+	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5, 'end': 1.1}, {'word': 'your', 'start': 1.1, 'end': 1.5}, {'word': 'best', 'start': 1.5, 'end': 1.8}, {'word': 'but', 'start': 1.8, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but', 'start': 9.4, 'end': 9.6}, {'word': 'not', 'start': 9.6, 'end': 9.7}, {'word': 'what', 'start': 9.7, 'end': 10.1}, {'word': 'you', 'start': 10.1, 'end': 10.5}, {'word': 'need', 'start': 10.5, 'end': 10.7}, {'word': 'when', 'start': 13.6, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 15.2}, {'word': 'so', 'start': 15.2, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.5}, {'word': 'you', 'start': 16.5, 'end': 16.8}, {'word': "can't", 'start': 16.8, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.5}, {'word': 'stuck', 'start': 17.5, 'end': 20.0}, {'word': 'in', 'start': 20.0, 'end': 21.3}, {'word': 'reverse', 'start': 21.3, 'end': 21.6}, {'word': 'and', 'start': 27.7, 'end': 28.5}, {'word': 'the', 'start': 28.5, 'end': 28.6}, {'word': 'tears', 'start': 28.6, 'end': 29.2}, {'word': 'comes', 'start': 29.2, 'end': 29.8}]
+	print(startEndTime)
 #	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5    , 'end': 1.0}, {'word': 'your', 'start': 1.0, 'end': 1.4}, {'word': 'best', 'start': 1.4, 'end': 1.7}, {'word': 'but', 'start': 1.7, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but'    , 'start': 9.4, 'end': 9.5}, {'word': 'not', 'start': 9.5, 'end': 9.8}, {'word': 'watching', 'start': 9.8, 'end': 10.5}, {'word': 'when', 'start': 14.0, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 14.9}, {'word': 'so', 'start': 14.9, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.6}, {'word': 'you', 'start': 16.6, 'end': 16.9}, {'word': "can't", 'start': 16.9, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.6}, {'word': 'cheer', 'start': 28.1, 'end': 29.2}, {'word': 'skirts', 'start': 29.2, 'end': 29.9}]
 #	lyrics = ["when you try your best but you don't succeed", 'when you get what you want but not watching', "when you feel so tired but you can't sleep", 'cheer skirts']
 
@@ -377,11 +356,11 @@ def runModel(songName, model):
 #	print(linesStartEnd)
 
 	if model == "onset":
-		modelTimes = onsetModel(filepath, startEndTime, lyrics)
+		modelTimes = onsetModel(filepath, linesStartEnd)
 	else:
-		modelTimes = approximate(startEndTime, lyrics)
+		modelTimes = approximate(linesStartEnd)
 
-#	print(modelTimes)
+	print(modelTimes)
 #	plot_audio_with_syllables(audio, modelTimes, songName)
 #	approx = [{'syll': 'when', 'start': 0.0, 'end': 0.4}, {'syll': 'you', 'start': 0.4, 'end': 0.5}, {'syll': 'try', 'start': 0.5, 'end': 1.0}, {'syll': 'your', 'start': 1.0, 'end': 1.4}, {'syll': 'best', 'start': 1.4, 'end': 1.7}, {'syll': 'but', 'start': 1.7, 'end': 2.3}, {'syll': 'you', 'start': 2.3, 'end': 2.5}, {'syll': "don't", 'start': 2.5, 'end': 2.7}, {'syll': 'suc', 'start': 2.7, 'end': 2.9000000000000004}, {'syll': 'ceed', 'start': 2.9000000000000004, 'end': 3.1}, {'syll': 'when', 'start': 7.0, 'end': 7.5}, {'syll': 'you', 'start': 7.5, 'end': 7.6}, {'syll': 'get', 'start': 7.6, 'end': 8.0}, {'syll': 'what', 'start': 8.0, 'end': 8.4}, {'syll': 'you', 'start': 8.4, 'end': 8.7}, {'syll': 'want', 'start': 8.7, 'end': 9.4}, {'syll': 'but', 'start': 9.4, 'end': 9.5}, {'syll': 'not', 'start': 9.5, 'end': 9.8}, {'syll': 'watch', 'start': 9.8, 'end': 10.15}, {'syll': 'ing', 'start': 10.15, 'end': 10.5}, {'syll': 'when', 'start': 14.0, 'end': 14.5}, {'syll': 'you', 'start': 14.5, 'end': 14.6}, {'syll': 'feel', 'start': 14.6, 'end': 14.9}, {'syll': 'so', 'start': 14.9, 'end': 15.6}, {'syll': 'tired', 'start': 15.6, 'end': 15.8}, {'syll': 'but', 'start': 15.8, 'end': 16.6}, {'syll': 'you', 'start': 16.6, 'end': 16.9}, {'syll': "can't", 'start': 16.9, 'end': 17.0}, {'syll': 'sleep', 'start': 17.0, 'end': 17.6}, {'syll': 'cheer', 'start': 28.1, 'end': 29.2}, {'syll': 'skirts', 'start': 29.2, 'end': 29.9}]
 #	approx = [((0.0, 0.4), 'when'), ((0.4, 0.5), 'you'), ((0.5, 1.0), 'try'), ((1.0, 1.4), 'your'), ((1.4, 1.7), 'best'), ((1.7, 2.3), 'but'), ((2.3, 2.5), 'you'), ((2.5, 2.7), "don't"), ((2.7, 2.9000000000000004), 'suc'), ((2.9000000000000004, 3.1), 'ceed'), ((7.0, 7.5), 'when'), ((7.5, 7.6), 'you'), ((7.6, 8.0), 'get'), ((8.0, 8.4), 'what'), ((8.4, 8.7), 'you'), ((8.7, 9.4), 'want'), ((9.4, 9.5), 'but'), ((9.5, 9.8), 'not'), ((9.8, 10.15), 'watch'), ((10.15, 10.5), 'ing'), ((14.0, 14.5), 'when'), ((14.5, 14.6), 'you'), ((14.6, 14.9), 'feel'), ((14.9, 15.6), 'so'), ((15.6, 15.8), 'tired'), ((15.8, 16.6), 'but'), ((16.6, 16.9), 'you'), ((16.9, 17.0), "can't"), ((17.0, 17.6), 'sleep'), ((28.1, 29.2), 'cheer'), ((29.2, 29.9), 'skirts')] 
