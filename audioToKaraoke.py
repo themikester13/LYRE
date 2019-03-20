a40e900628fc664bd49d5f47ea14ed1282837fa6
themikester13 <mjiang321@gmail.com>
2019-03-20 17:25:28 -0500

    Did i do something?

--- audioToKaraoke.py
+++ audioToKaraoke.py
@@ -311,12 +311,21 @@ def plot_audio_with_syllables(audio, syllables, title, word=None):
 	
 	plt.show()
 
+#processes output of start end time for better display
+def newEndTime(startEndTime):
+	#print(startEndTime[:-1])
+	replacedEndTimes = []
+	for i, syll in enumerate(startEndTime[:-1]):
+		replacedEndTimes.append(((syll[0][0], startEndTime[i+1][0][0]), syll[1]))
+	replacedEndTimes.append(startEndTime[-1])	
+	return replacedEndTimes
+
 #adds lyrics to the Sound Video File
 def addSubtitles(startEndTimes, filepath, filename, vidLen):
 	video = VideoFileClip(filepath)
 	currVideo = video
 	generator = lambda text: TextClip(text, font="Times", fontsize=150, color='white').set_pos('center')
-	sub = SubtitlesClip(startEndTimes, generator)
+	sub = SubtitlesClip(newEndTime(startEndTimes), generator)
 	final = CompositeVideoClip([video, sub.set_pos(('center', 'center'))])
 	path = './movieFiles/Karaoke/' + filename + '.avi'
 	final.write_videofile(path, codec = 'libx264', audio_codec = 'pcm_s32le', fps = 24)
@@ -344,15 +353,15 @@ def runModel(songName, model):
 	# plot_audio(audio, sr, figsize=(16,4), title="I'm Yours")
 
 	# execute transcription
-#	startEndTime, lyrics = getSpeechInfo(filepath)
-	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5, 'end': 1.1}, {'word': 'your', 'start': 1.1, 'end': 1.5}, {'word': 'best', 'start': 1.5, 'end': 1.8}, {'word': 'but', 'start': 1.8, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but', 'start': 9.4, 'end': 9.6}, {'word': 'not', 'start': 9.6, 'end': 9.7}, {'word': 'what', 'start': 9.7, 'end': 10.1}, {'word': 'you', 'start': 10.1, 'end': 10.5}, {'word': 'need', 'start': 10.5, 'end': 10.7}, {'word': 'when', 'start': 13.6, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 15.2}, {'word': 'so', 'start': 15.2, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.5}, {'word': 'you', 'start': 16.5, 'end': 16.8}, {'word': "can't", 'start': 16.8, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.5}, {'word': 'stuck', 'start': 17.5, 'end': 20.0}, {'word': 'in', 'start': 20.0, 'end': 21.3}, {'word': 'reverse', 'start': 21.3, 'end': 21.6}, {'word': 'and', 'start': 27.7, 'end': 28.5}, {'word': 'the', 'start': 28.5, 'end': 28.6}, {'word': 'tears', 'start': 28.6, 'end': 29.2}, {'word': 'comes', 'start': 29.2, 'end': 29.8}]
-	print(startEndTime)
+	startEndTime, lyrics = getSpeechInfo(filepath)
+#	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5, 'end': 1.1}, {'word': 'your', 'start': 1.1, 'end': 1.5}, {'word': 'best', 'start': 1.5, 'end': 1.8}, {'word': 'but', 'start': 1.8, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but', 'start': 9.4, 'end': 9.6}, {'word': 'not', 'start': 9.6, 'end': 9.7}, {'word': 'what', 'start': 9.7, 'end': 10.1}, {'word': 'you', 'start': 10.1, 'end': 10.5}, {'word': 'need', 'start': 10.5, 'end': 10.7}, {'word': 'when', 'start': 13.6, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 15.2}, {'word': 'so', 'start': 15.2, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.5}, {'word': 'you', 'start': 16.5, 'end': 16.8}, {'word': "can't", 'start': 16.8, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.5}, {'word': 'stuck', 'start': 17.5, 'end': 20.0}, {'word': 'in', 'start': 20.0, 'end': 21.3}, {'word': 'reverse', 'start': 21.3, 'end': 21.6}, {'word': 'and', 'start': 27.7, 'end': 28.5}, {'word': 'the', 'start': 28.5, 'end': 28.6}, {'word': 'tears', 'start': 28.6, 'end': 29.2}, {'word': 'comes', 'start': 29.2, 'end': 29.8}]
+#	print(startEndTime)
 #	startEndTime = [{'word': 'when', 'start': 0.0, 'end': 0.4}, {'word': 'you', 'start': 0.4, 'end': 0.5}, {'word': 'try', 'start': 0.5    , 'end': 1.0}, {'word': 'your', 'start': 1.0, 'end': 1.4}, {'word': 'best', 'start': 1.4, 'end': 1.7}, {'word': 'but', 'start': 1.7, 'end': 2.3}, {'word': 'you', 'start': 2.3, 'end': 2.5}, {'word': "don't", 'start': 2.5, 'end': 2.7}, {'word': 'succeed', 'start': 2.7, 'end': 3.1}, {'word': 'when', 'start': 7.0, 'end': 7.5}, {'word': 'you', 'start': 7.5, 'end': 7.6}, {'word': 'get', 'start': 7.6, 'end': 8.0}, {'word': 'what', 'start': 8.0, 'end': 8.4}, {'word': 'you', 'start': 8.4, 'end': 8.7}, {'word': 'want', 'start': 8.7, 'end': 9.4}, {'word': 'but'    , 'start': 9.4, 'end': 9.5}, {'word': 'not', 'start': 9.5, 'end': 9.8}, {'word': 'watching', 'start': 9.8, 'end': 10.5}, {'word': 'when', 'start': 14.0, 'end': 14.5}, {'word': 'you', 'start': 14.5, 'end': 14.6}, {'word': 'feel', 'start': 14.6, 'end': 14.9}, {'word': 'so', 'start': 14.9, 'end': 15.6}, {'word': 'tired', 'start': 15.6, 'end': 15.8}, {'word': 'but', 'start': 15.8, 'end': 16.6}, {'word': 'you', 'start': 16.6, 'end': 16.9}, {'word': "can't", 'start': 16.9, 'end': 17.0}, {'word': 'sleep', 'start': 17.0, 'end': 17.6}, {'word': 'cheer', 'start': 28.1, 'end': 29.2}, {'word': 'skirts', 'start': 29.2, 'end': 29.9}]
 #	lyrics = ["when you try your best but you don't succeed", 'when you get what you want but not watching', "when you feel so tired but you can't sleep", 'cheer skirts']
 
 	# separate into lines
 	linesStartEnd, lines = generateLines(audio, startEndTime)
-	print("Generated Lines:", lines)
+#	print("Generated Lines:", lines)
 #	print(linesStartEnd)
 
 	if model == "onset":
@@ -360,7 +369,7 @@ def runModel(songName, model):
 	else:
 		modelTimes = approximate(linesStartEnd)
 
-	print(modelTimes)
+	print(newEndTime(modelTimes))
 #	plot_audio_with_syllables(audio, modelTimes, songName)
 #	approx = [{'syll': 'when', 'start': 0.0, 'end': 0.4}, {'syll': 'you', 'start': 0.4, 'end': 0.5}, {'syll': 'try', 'start': 0.5, 'end': 1.0}, {'syll': 'your', 'start': 1.0, 'end': 1.4}, {'syll': 'best', 'start': 1.4, 'end': 1.7}, {'syll': 'but', 'start': 1.7, 'end': 2.3}, {'syll': 'you', 'start': 2.3, 'end': 2.5}, {'syll': "don't", 'start': 2.5, 'end': 2.7}, {'syll': 'suc', 'start': 2.7, 'end': 2.9000000000000004}, {'syll': 'ceed', 'start': 2.9000000000000004, 'end': 3.1}, {'syll': 'when', 'start': 7.0, 'end': 7.5}, {'syll': 'you', 'start': 7.5, 'end': 7.6}, {'syll': 'get', 'start': 7.6, 'end': 8.0}, {'syll': 'what', 'start': 8.0, 'end': 8.4}, {'syll': 'you', 'start': 8.4, 'end': 8.7}, {'syll': 'want', 'start': 8.7, 'end': 9.4}, {'syll': 'but', 'start': 9.4, 'end': 9.5}, {'syll': 'not', 'start': 9.5, 'end': 9.8}, {'syll': 'watch', 'start': 9.8, 'end': 10.15}, {'syll': 'ing', 'start': 10.15, 'end': 10.5}, {'syll': 'when', 'start': 14.0, 'end': 14.5}, {'syll': 'you', 'start': 14.5, 'end': 14.6}, {'syll': 'feel', 'start': 14.6, 'end': 14.9}, {'syll': 'so', 'start': 14.9, 'end': 15.6}, {'syll': 'tired', 'start': 15.6, 'end': 15.8}, {'syll': 'but', 'start': 15.8, 'end': 16.6}, {'syll': 'you', 'start': 16.6, 'end': 16.9}, {'syll': "can't", 'start': 16.9, 'end': 17.0}, {'syll': 'sleep', 'start': 17.0, 'end': 17.6}, {'syll': 'cheer', 'start': 28.1, 'end': 29.2}, {'syll': 'skirts', 'start': 29.2, 'end': 29.9}]
 #	approx = [((0.0, 0.4), 'when'), ((0.4, 0.5), 'you'), ((0.5, 1.0), 'try'), ((1.0, 1.4), 'your'), ((1.4, 1.7), 'best'), ((1.7, 2.3), 'but'), ((2.3, 2.5), 'you'), ((2.5, 2.7), "don't"), ((2.7, 2.9000000000000004), 'suc'), ((2.9000000000000004, 3.1), 'ceed'), ((7.0, 7.5), 'when'), ((7.5, 7.6), 'you'), ((7.6, 8.0), 'get'), ((8.0, 8.4), 'what'), ((8.4, 8.7), 'you'), ((8.7, 9.4), 'want'), ((9.4, 9.5), 'but'), ((9.5, 9.8), 'not'), ((9.8, 10.15), 'watch'), ((10.15, 10.5), 'ing'), ((14.0, 14.5), 'when'), ((14.5, 14.6), 'you'), ((14.6, 14.9), 'feel'), ((14.9, 15.6), 'so'), ((15.6, 15.8), 'tired'), ((15.8, 16.6), 'but'), ((16.6, 16.9), 'you'), ((16.9, 17.0), "can't"), ((17.0, 17.6), 'sleep'), ((28.1, 29.2), 'cheer'), ((29.2, 29.9), 'skirts')] 
